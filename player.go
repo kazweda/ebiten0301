4b14410029ef0dad0741d88cc8f26c2ac8a228cc
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2025-03-01 15:17:52 +0900

    playerの描画 #8

---
+++ player.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+// プレイヤーの設定
+const (
+	playerWidth    = 80                                  // プレイヤーの幅
+	playerHeight   = 10                                  // プレイヤーの高さ
+	initialPlayerX = (gameScreenWidth - playerWidth) / 2 // プレイヤーの初期位置
+	initialPlayerY = gameScreenHeight - playerHeight     // プレイヤーの初期位置
+)
+
+type Player struct {
+	x, y   float64
+	width  float64
+	height float64
+	img    *ebiten.Image
+}
+
+func NewPlayer() *Player {
+	img := ebiten.NewImage(int(playerWidth), int(playerHeight))
+	img.Fill(color.White)
+
+	return &Player{
+		x:      initialPlayerX,
+		y:      initialPlayerY,
+		width:  playerWidth,
+		height: playerHeight,
+		img:    img,
+	}
+}
